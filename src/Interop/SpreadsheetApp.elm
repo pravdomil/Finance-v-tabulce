e96403f6b7836d4ec09a5e3abd90ae1125976b9e
pravdomil <pravdomil.toman@gmail.com>
2021-03-08 11:08:07 +0100

    use JavaScript.decode

--- src/Interop/SpreadsheetApp.elm
+++ src/Interop/SpreadsheetApp.elm
@@ -4,10 +4,10 @@ import Interop.JavaScript as JavaScript exposing (Exception)
 import Interop.Spreadsheet exposing (Spreadsheet(..))
 import Json.Decode as Decode
 import Task exposing (Task)
-import Utils.Task_ as Task_
 
 
 active : Task Exception (Maybe Spreadsheet)
 active =
-    JavaScript.run "SpreadsheetApp.getActive()"
-        |> Task_.andThenDecode (Decode.nullable (Decode.map Spreadsheet Decode.value))
+    "SpreadsheetApp.getActive()"
+        |> JavaScript.run
+        |> JavaScript.decode (Decode.nullable (Decode.map Spreadsheet Decode.value))
